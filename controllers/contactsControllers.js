5ce0c6a8fa38626b3bb1b5ddd285fab390f49a90
kouk02 <kouk20020703@gmail.com>
2024-02-11 21:34:26 +0200

    add contactsServices

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,10 @@
-import contactsService from "../services/contactsServices.js";
+// import contactsService from "../services/contactsServices.js";
+const contactsService = require('../services/contactsServices');
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = (req, res) => {
+    const allContacts = contactsService.listContacts();
+    res.status(200).json(allContacts);
+};
 
 export const getOneContact = (req, res) => {};
 
